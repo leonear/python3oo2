a98c087507ff57765b61031ed93c1423671d9dea
Leonardo Aparicio <lsilvaaparicio@gmail.com>
2023-08-01 07:30:03 -0300

    criação da lista filmes_e_series e criação de um for que verifica a função programa na lista para imprimir os filmes e series contidos na lista criada de forma otimizada

--- modelo.py
+++ modelo.py
@@ -36,11 +36,14 @@ class Serie(Programa):
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}'
-      f'- Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
-
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}'
-      f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
